288b165b3d440295e1f41884e22afd26a01072df
German Carrillo <german.carillo@aurea.com>
2019-02-08 16:25:05 -0500

    upload of travis.yml

--- frontend/script.sh
+++ frontend/script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apiUrl="//[A-Za-z0-9_.]+"
-replaceUrl="//localhost"
+replaceUrl="//${HOSTNAME}"
 filename="./src/assets/config/configuration.ts"
 echo sed -i "s/($apiUrl)/(${replaceUrl})/g" "$filename"
